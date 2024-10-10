ff799f0f4f220ac10979d5400b35484f11ebca47
petergt44 <petergt44@gmail.com>
2024-10-11 02:10:41 +0300

    Fix API authentication issue

--- flights-react-app/src/api/flights.js
+++ flights-react-app/src/api/flights.js
@@ -1,18 +1,18 @@
 import axios from 'axios';
 
-const options = {
-  method: 'GET',
-  url: 'https://sky-scrapper.p.rapidapi.com/api/search',
-  params: { from: '', to: '', date: '' },
-  headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY,
-    'X-RapidAPI-Host': 'sky-scrapper.p.rapidapi.com'
-  }
-};
-
 export const fetchFlights = async (from, to, date) => {
+  const options = {
+    method: 'GET',
+    url: 'https://sky-scrapper.p.rapidapi.com/api/search',
+    params: { from, to, date },
+    headers: {
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY,
+      'X-RapidAPI-Host': 'sky-scrapper.p.rapidapi.com'
+    }
+  };
+
   try {
-    const response = await axios.request({ ...options, params: { from, to, date } });
+    const response = await axios.request(options);
     return response.data;
   } catch (error) {
     console.error('Error fetching flight data:', error);
